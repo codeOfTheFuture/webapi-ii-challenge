b897c11dc30d919f0f8a0f0c42676f33263ba628
Jeff Oliver <jeffoliver3384@gmail.com>
2019-07-09 17:24:26 -0400

    created endpoints using express router for adding a post and updating a post

--- data/dbRouter.js
+++ data/dbRouter.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.use(express.json());
 
+// Get all posts
 router.get('/', async (req, res) => {
   try {
     const posts = await db.find(req.query);
@@ -18,11 +19,55 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {});
+// Get single post by id
+router.get('/:id', async (req, res) => {
+  console.log(`hit /:id with ${req.id}`);
+  try {
+    const post = await db.findById(req.params.id);
 
-router.post('/', async (req, res) => {});
+    if (post && post.length) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: 'Post not found' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Error retrieving the requested post',
+    });
+  }
+});
 
-router.put('/:id', async (req, res) => {});
+// Add a post
+router.post('/', async (req, res) => {
+  try {
+    const post = await db.insert(req.body);
+    console.log('post body: ', req.body);
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(500).json({
+      message: 'There was an error adding the post',
+    });
+  }
+});
+
+// Update a post
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedPost = await db.update(req.params.id, req.body);
+    if (updatedPost) {
+      res.status(200).json(updatedPost);
+    } else {
+      res.status(404).json({
+        message: 'The post you want to update cannot be found',
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'There was an error updating the post',
+    });
+  }
+});
 
 router.delete('/:id', async (req, res) => {});
 
